3aa7c0e945d7bd7f5777c72ce316345862a97610
artemtimofeev <timofeev.ae@phystech.edu>
2022-02-24 01:27:39 +0300

    added threading

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import config
-from modules.utils.message import Message
+import threading
 from modules.handlers import Handler
 from modules.message_queue import MessageQueue
 from modules.bot import Bot
 
 
-def start():
-    bot = Bot(token=config.API_KEY)
-
-    message_queue = MessageQueue()
-
-    users = {}
-
+def check_messages(bot, message_queue, users):
     while True:
         new_message = bot.get_new_message()
         if new_message is not None and new_message.user_id in [182040882, 348350925, 406197915, 579989535]:
@@ -20,10 +14,27 @@ def start():
             response = users[new_message.user_id].answer(new_message)
             message_queue.push(response)
 
-            messages_to_send = message_queue.pop()
-            for message in messages_to_send:
-                bot.send_message(message)
+
+def send_messages(bot, message_queue):
+    while True:
+        messages_to_send = message_queue.pop()
+        for message in messages_to_send:
+            bot.send_message(message)
 
 
 if __name__ == "__main__":
-    start()
+    bot = Bot(token=config.API_KEY)
+    message_queue = MessageQueue()
+    users = {}
+
+    checking_thread = threading.Thread(target=check_messages, args=(bot, message_queue, users))
+    answering_thread = threading.Thread(target=send_messages, args=(bot, message_queue))
+
+    checking_thread.start()
+    answering_thread.start()
+
+    stop = input("Остановить?")
+
+    if stop == "yes":
+        checking_thread.join()
+        answering_thread.join()
--- modules/message_queue.py
+++ modules/message_queue.py
@@ -1,3 +1,4 @@
+import threading
 from datetime import datetime
 from heapq import heappush as insert, heappop as extract_maximum
 
@@ -5,16 +6,22 @@ from heapq import heappush as insert, heappop as extract_maximum
 class MessageQueue:
     def __init__(self):
         self.queue = []
+        self.lock = threading.Lock()
 
     def push(self, message):
+        self.lock.acquire()
         insert(self.queue, message)
+        self.lock.release()
 
     def pop(self):
+        self.lock.acquire()
         messages_to_send = []
         current_time = datetime.timestamp(datetime.now())
 
         while (len(self.queue) > 0
                and self.queue[0].send_time < current_time):
             messages_to_send.append(extract_maximum(self.queue))
+
+        self.lock.release()
 
         return messages_to_send
